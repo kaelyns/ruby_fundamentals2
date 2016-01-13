7a7360d1eb1a0a68ec900ded7d9675df10ea5f18
kaelynsuh <kaelyn.suh@gmail.com>
2016-01-13 14:57:28 -0500

    check for bananas

--- exercise6.rb
+++ exercise6.rb
@@ -3,13 +3,27 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
 def finallist(grocery_list)
+    bananas = 0
+
     grocery_list.each do |item|
         puts "* #{item}"
+        if item == "bananas"
+            bananas = 1
+        end
     end
 
-    puts "Total number of items: #{grocery_list.count}"
+    total = grocery_list.count
+    puts "Total number of items: #{total}"
+
+    if bananas == 1
+        puts "You dont need to pick up bananas today."
+    else
+        puts "You need to pick up bananas."
+    end
+
+
+    # for grocery_list.
 end
 
 grocery_list << "rice"
-
 finallist(grocery_list)
