a05b10a12fd5a920ad0bc0938d0cd538f9813b13
kaelynsuh <kaelyn.suh@gmail.com>
2016-01-13 14:44:12 -0500

    add rice

--- exercise6.rb
+++ exercise6.rb
@@ -2,6 +2,12 @@
 
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-grocery_list.each do |item|
-    puts "* #{item}"
+def finallist(grocery_list)
+    grocery_list.each do |item|
+        puts "* #{item}"
+    end
 end
+
+grocery_list << "rice"
+
+finallist(grocery_list)
